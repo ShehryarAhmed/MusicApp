8077e181a30cfcbd4defaba71e050984965edea9
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-03-05 20:10:33 +0500

    add recycler view

---
+++ CardView/app/src/main/java/com/example/android/cardview/MainActivity.java
@@ -0,0 +1,29 @@
+package com.example.android.cardview;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+public class MainActivity extends AppCompatActivity {
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+    }
+}
---
+++ CardView/app/src/main/java/com/example/android/cardview/MyAdapter.java
@@ -0,0 +1,8 @@
+package com.example.android.cardview;
+
+/**
+ * Created by android on 3/5/2017.
+ */
+
+public class MyAdapter  {
+}
