d98dd351b31a0c492b75d97b3f6573fdfdaf2be8
shehryarAhmed <shehreyarahmed5@gmail.com>
2017-03-06 00:11:04 +0500

    fixed error remove setToolbarMethod

--- CardView/app/src/main/java/com/example/android/cardview/AlbumsAdapter.java
+++ CardView/app/src/main/java/com/example/android/cardview/AlbumsAdapter.java
@@ -13,7 +13,7 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import com.bumptech.glide.Glide;
-import com.example.android.cardview.model.AlbumDetail;
+import com.example.android.cardview.model.Album;
 
 import java.util.List;
 
@@ -21,10 +21,10 @@ import java.util.List;
  * Created by android on 3/5/2017.
  */
 
-public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
+public class AlbumsAdapter extends RecyclerView.Adapter<AlbumsAdapter.ViewHolder> {
     private String[] mDataset;
     private Context mContext;
-    private List<AlbumDetail> albumDetailList;
+    private List<Album> albumList;
 
     public static class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         // each data item is just a string in this case
@@ -49,13 +49,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
         }
     }
 
-    public MyAdapter(Context context, List<AlbumDetail> lists) {
+    public AlbumsAdapter(Context context, List<Album> lists) {
         mContext = context;
-        albumDetailList = lists;
+        albumList = lists;
     }
 
     @Override
-    public MyAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+    public AlbumsAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         // create a new view
         View v = LayoutInflater.from(parent.getContext()).inflate(R.layout.activity_albumcard, parent, false);
         // set the view's size, margins, paddings and layout parameters
@@ -64,13 +64,13 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
     }
 
     @Override
-    public void onBindViewHolder(final MyAdapter.ViewHolder holder, int position) {
+    public void onBindViewHolder(final AlbumsAdapter.ViewHolder holder, int position) {
 
-        AlbumDetail albumDetail = albumDetailList.get(position);
-        holder.mTitle.setText(albumDetail.getName());
-        holder.mCount.setText(albumDetail.getNumOfSongs() + " Songs");
+        Album album = albumList.get(position);
+        holder.mTitle.setText(album.getName());
+        holder.mCount.setText(album.getNumOfSongs() + " Songs");
 
-        Glide.with(mContext).load(albumDetail.getThumbnail()).into(holder.mThumbnail);
+        Glide.with(mContext).load(album.getThumbnail()).into(holder.mThumbnail);
         holder.mOverflow.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -113,6 +113,6 @@ public class MyAdapter extends RecyclerView.Adapter<MyAdapter.ViewHolder> {
 
     @Override
     public int getItemCount() {
-        return albumDetailList.size();
+        return albumList.size();
     }
 }
--- CardView/app/src/main/java/com/example/android/cardview/model/Album.java
+++ CardView/app/src/main/java/com/example/android/cardview/model/Album.java
@@ -4,14 +4,14 @@ package com.example.android.cardview.model;
  * Created by android on 3/5/2017.
  */
 
-public class AlbumDetail {
+public class Album {
     private String name;
     private int numOfSongs;
     private int thumbnail;
 
-    public AlbumDetail(){}
+    public Album(){}
 
-    public AlbumDetail(int numOfSongs, String name, int thumbnail) {
+    public Album(String name, int numOfSongs, int thumbnail) {
         this.numOfSongs = numOfSongs;
         this.name = name;
         this.thumbnail = thumbnail;
